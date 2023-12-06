5c700f74c80c02f501b8b80c31a3c8bf1c06cbcc
Zemorath <trentward10@gmail.com>
2023-12-05 22:05:52 -0600

    Attributes and Methods Lab Complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        self.genres.append(genre)
+        self.artists.append(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
